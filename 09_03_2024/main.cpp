2d2f551df58fa4a931dd2edc1afb2165aa4c2e54
Angela Venable <avenable11@ivytech.edu>
2024-09-03 17:18:50 -0400

    added loops

--- 09_03_2024/main.cpp
+++ 09_03_2024/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <limits>
 
 int main()
 {
@@ -8,6 +9,13 @@ int main()
     int userNum;
     std::cout << "Enter a number between 1 and 100: ";
     std::cin >> userNum;
+    while (!std::cin)
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Enter a number between 1 and 100: ";
+        std::cin >> userNum;
+    }
 
     if (magicNum >= userNum)
     {
@@ -28,6 +36,36 @@ int main()
         std::cout << "You guessed 20!" << std::endl;
     case 30:
         std::cout << "you guessed 30!" << std::endl;
+    }
+
+    std::ifstream inFile;
+    inFile.open("data.txt");
+    if (!inFile.is_open())
+    {
+        std::cout << "The input file could not be opened." << std::endl;
+        return 0;
+    }
+    // std::cout << "Enter a new magic num"
+    while (!inFile.eof())
+    {
+        inFile >> magicNum;
+        std::cout << "Pick a new number between 1 and 100: ";
+        std::cin >> userNum;
+        while (!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Enter a number between 1 and 100: ";
+            std::cin >> userNum;
+        }
+        if (magicNum >= userNum)
+        {
+            std::cout << "You lose. My number is bigger so I win!!!!!!" << std::endl;
+        }
+        else
+        {
+            std::cout << "That was fun!" << std::endl;
+        }
     }
 
     return 0;
